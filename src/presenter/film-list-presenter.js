b96c6b2a9d7f43a260e77ed38b48be87028fa614
Konung <mreader212@gmail.com>
2022-06-07 20:34:52 +0300

    Реализует кнопку "Show More" и отображает сообщение об отсутствии фильмов, если их список пуст

--- src/presenter/film-list-presenter.js
+++ src/presenter/film-list-presenter.js
@@ -1,14 +1,20 @@
 import FilmCardView from '../view/film-card-view.js';
 import FilmListView from '../view/film-list-view.js';
+import NoFilmsListView from '../view/no-films-list-view.js';
 import ShowMoreButtonView from '../view/show-more-button-view.js';
 import SortingView from '../view/sorting-view.js';
 import FilmContainerView from '../view/film-container-view.js';
 import PopupView from '../view/popup-view.js';
 import {render} from '../render.js';
 
+const FILMS_COUNT_PER_STEP = 5;
+
 export default class FilmListPresenter {
+  #noFilmsListComponent = new NoFilmsListView();
   #filmListComponent = new FilmListView();
+  #showMoreButtonComponent = new ShowMoreButtonView();
   #filmContainer = new FilmContainerView();
+  #renderedFilmCardsAmount = FILMS_COUNT_PER_STEP;
   #filteredComments = [];
   #filmsModel = null;
   #films = null;
@@ -23,21 +29,43 @@ export default class FilmListPresenter {
     this.#comments = [...this.#commentsModel.comments];
 
     render(new SortingView(), document.querySelector('.main-navigation'), 'afterend');
-    render(this.#filmListComponent, pageMainElement);
-    render(this.#filmContainer, this.#filmListComponent.element.querySelector('.films-list'));
+    if(this.#films.length){
+      render(this.#filmListComponent, pageMainElement);
+      render(this.#filmContainer, this.#filmListComponent.element.querySelector('.films-list'));
 
-    for (let i = 0; i < this.#films.length; i++) {
+      for (let i = 0; i < Math.min(FILMS_COUNT_PER_STEP, this.#films.length); i++) {
+        this.#filteredComments.push(this.#filterComments(this.#films[i], this.#comments));
+        this.#renderFilmCard(this.#films[i], this.#filteredComments[i]);
+      }
+
+      if(this.#films.length > FILMS_COUNT_PER_STEP) {
+        render(this.#showMoreButtonComponent, this.#filmListComponent.element.querySelector('.films-list'));
+
+        this.#showMoreButtonComponent.element.addEventListener('click', this.#onShowMoreButtonClick);
+      }
+    } else {
+      render(this.#noFilmsListComponent, pageMainElement);
+    }
+  };
+
+  #onShowMoreButtonClick = () => {
+    for (let i = this.#renderedFilmCardsAmount; i < this.#renderedFilmCardsAmount
+      + Math.min(FILMS_COUNT_PER_STEP, this.#films.length - this.#renderedFilmCardsAmount); i++) {
       this.#filteredComments.push(this.#filterComments(this.#films[i], this.#comments));
-      this.#renderFilmCards(this.#films[i], this.#filteredComments[i]);
+      this.#renderFilmCard(this.#films[i], this.#filteredComments[i]);
     }
 
-    render(new ShowMoreButtonView(), this.#filmListComponent.element.querySelector('.films-list'));
+    this.#renderedFilmCardsAmount += FILMS_COUNT_PER_STEP;
 
+    if (this.#renderedFilmCardsAmount >= this.#films.length) {
+      this.#showMoreButtonComponent.element.remove();
+      this.#showMoreButtonComponent.removeElement();
+    }
   };
 
   #filterComments = (film, comments) => comments.filter((comment) => film.info.commentIDs.includes(comment.id));
 
-  #renderFilmCards = (film, comments) => {
+  #renderFilmCard = (film, comments) => {
     const filmCardComponent = new FilmCardView(film);
     const documentBody = document.querySelector('body');
 
